126e43ee5f58ef757096a739e646a0cea265898a
iamkorir3 <korir5956@gmail.com>
2025-08-30 23:27:07 +0300

    adding Homepage content

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,14 +3,43 @@ export default function Homepage() {
   return (
     <div className={styles.container}>
       <Mydetails />
+      <PictureSkills />
     </div>
   );
 }
 
 function Mydetails() {
   return (
-    <div>
-      <h1>i am Korir Emmanuel</h1>
+    <div className={styles.detailContainer}>
+      <h1>
+        {" "}
+        Hi, I Am Korir <br />
+        Emmanuel
+      </h1>
+      <h2>Frontend Developer</h2>
+      <p>
+        I build modern, responsive web apps with clean UI and smooth <br />
+        Ux blending design and code to create experiences that feel intuitive,
+        <br />
+        fast and delightful to use.
+      </p>
+      <SocialProfiles />
+    </div>
+  );
+}
+
+function PictureSkills() {
+  return (
+    <div className={styles.profile}>
+      <img src="/me3.jpg" alt="my pic" />
+    </div>
+  );
+}
+
+function SocialProfiles() {
+  return (
+    <div className={styles.SocialProfiles}>
+      <p>Follow me on: </p>
     </div>
   );
 }
