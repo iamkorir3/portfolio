492f7be70d7f58b72fa45ac2276468bfcfe5833b
iamkorir3 <korir5956@gmail.com>
2025-09-04 20:18:30 +0300

    personal info

--- src/components/Aboutcompo.jsx
+++ src/components/Aboutcompo.jsx
@@ -1,10 +1,16 @@
 import styles from "./Aboutcompo.module.css";
 export default function About() {
   return (
-    <div className={styles.container}>
-      <Picture />
-      <InfoMe />
-    </div>
+    <>
+      <div className={styles.infoTitle}>
+        <h2>About Me</h2>
+        <p>Discover More about me, story behind my Work</p>
+      </div>
+      <div className={styles.container}>
+        <Picture />
+        <InfoMe />
+      </div>
+    </>
   );
 }
 
@@ -18,7 +24,7 @@ function Picture() {
 
 function InfoMe() {
   return (
-    <>
+    <div>
       <div className={styles.myInfo}>
         <div>
           <h2>
@@ -36,35 +42,37 @@ function InfoMe() {
             <ion-icon name="bulb-outline"></ion-icon> My Approach
           </h2>
           <p>
-            I am a frontend developer who crafts responsive, interactive and
-            clean UIs. <br></br>
-            With a background in Computer Technology (still student). i blend
-            design sense with technical logic.
+            I focus on user-friendly design, <br></br> performance, and
+            accessability always<br></br> evolving around modern techs to
+            deliver <br></br>
+            seamless Experience in my work.
           </p>
         </div>
       </div>
 
-      <div>
+      <div className={styles.personalInfo}>
         <h2>Personal Information</h2>
-        <p>
-          Name<span>Korir Emmanuel</span>
-        </p>
-        <p>
-          place of birth<span>Bomet</span>
-        </p>
-        <p>
-          phone<span>+254 712473905</span>
-        </p>
-        <p>
-          Date of birth<span></span>
-        </p>
-        <p>
-          Emai<span>iamkorir705@gmail.com</span>
-        </p>
-        <p>
-          Education<span> Meru University of Science and Technoly</span>
-        </p>
+        <div className={styles.myPersonalInfo}>
+          <p>
+            Name: <span>Korir Emmanuel</span>
+          </p>
+          <p>
+            place of birth: <span>Bomet, Kenya</span>
+          </p>
+          <p>
+            phone: <span>+254 712473905</span>
+          </p>
+          <p>
+            Date of birth: <span>05/01/2006</span>
+          </p>
+          <p>
+            Email: <span>iamkorir705@gmail.com</span>
+          </p>
+          <p>
+            Education: <span> Meru University of Science and Technology</span>
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
