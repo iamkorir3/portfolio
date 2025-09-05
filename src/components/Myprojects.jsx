a12a0b41ed1c2c8962e6620937bfb8de3083103e
iamkorir3 <korir5956@gmail.com>
2025-09-05 23:24:50 +0300

    finished projects layout

--- src/components/Myprojects.jsx
+++ src/components/Myprojects.jsx
@@ -1,5 +1,68 @@
+import { Children } from "react";
 import styles from "./Myprojects.module.css";
 
 export default function Myprojects() {
-  return <div className={styles.container}>projects</div>;
+  return (
+    <div className={styles.container}>
+      <Projects />
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div className={styles.projects}>
+      <ProjectCard>
+        <h2>WorldWise</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio
+          cupiditate et exercitationem! Veniam asperiores culpa fuga sed ipsum,
+          .
+        </p>
+      </ProjectCard>
+      <ProjectCard>
+        <h2>Ecommerce</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio
+          cupiditate et exercitationem! Veniam asperiores culpa fuga sed ipsum,
+          .
+        </p>
+      </ProjectCard>
+      <ProjectCard>
+        <h2>UsePopcorn</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio
+          cupiditate et exercitationem! Veniam asperiores culpa fuga sed ipsum,
+          .
+        </p>
+      </ProjectCard>
+      <ProjectCard>
+        <h2>React Quiz</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio
+          cupiditate et exercitationem! Veniam asperiores culpa fuga sed ipsum,
+          .
+        </p>
+      </ProjectCard>
+      <ProjectCard>
+        <h2>Eat and Split</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio
+          cupiditate et exercitationem! Veniam asperiores culpa fuga sed ipsum,
+          .
+        </p>
+      </ProjectCard>
+      <ProjectCard>
+        <h2>To Do List</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio
+          cupiditate et exercitationem! Veniam asperiores culpa fuga sed ipsum,
+          .
+        </p>
+      </ProjectCard>
+    </div>
+  );
+}
+function ProjectCard({ children }) {
+  return <div className={styles.projectsCard}>{children}</div>;
 }
