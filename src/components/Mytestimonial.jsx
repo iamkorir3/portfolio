b2cde32f4a19a438d37f51e2623f5a8c2ba4a21b
iamkorir3 <korir5956@gmail.com>
2025-09-08 23:14:14 +0300

    finished creating testimonial UI

--- src/components/Mytestimonial.jsx
+++ src/components/Mytestimonial.jsx
@@ -9,6 +9,7 @@ export default function Mytestimonial() {
           experienced my work.
         </p>
       </div>
+      <AddTestimonial />
       <TestimonialCard />
     </div>
   );
@@ -18,9 +19,23 @@ function TestimonialCard() {
   return (
     <div className={styles.tesiCard}>
       <PersonCard />
+      {/* <PersonCard />
       <PersonCard />
       <PersonCard />
-      <PersonCard />
+      <PersonCard /> */}
+    </div>
+  );
+}
+function AddTestimonial() {
+  return (
+    <div className={styles.testmonialBtn}>
+      <h3>
+        <ion-icon name="mail-outline"></ion-icon> Testimonials
+      </h3>
+      <button>
+        + {/* <ion-icon name="add-circle-outline"></ion-icon> */}
+        Add Testimonial
+      </button>
     </div>
   );
 }
@@ -28,7 +43,30 @@ function TestimonialCard() {
 function PersonCard() {
   return (
     <div className={styles.personCard}>
-      <h2>Name: korir emmanuel</h2>
+      <div className={styles.review}>
+        <h3>
+          <ion-icon name="chatbubble-ellipses-outline"></ion-icon> Testimony
+        </h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est,
+          pariatur maiores. Voluptate incidunt aut dolorum.
+        </p>
+        <p className={styles.ratings}>⭐⭐⭐⭐⭐</p>
+      </div>
+      <div className={styles.testifier}>
+        <div className={styles.nameInitials}>KE</div>
+        <div>
+          <h2>korir emmanuel</h2>
+          <p>designation</p>
+        </div>
+      </div>
     </div>
   );
+}
+
+// **************
+// Testimonial Form
+
+function Form() {
+  return;
 }
