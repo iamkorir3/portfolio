22e3aeb35b12b3f15097e6c47bfc8270d53be274
iamkorir3 <korir5956@gmail.com>
2025-09-10 20:38:07 +0300

    working on testimonial form

--- src/components/Mytestimonial.jsx
+++ src/components/Mytestimonial.jsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
 import styles from "./Mytestimonial.module.css";
 
-const testifiers = [
-  { name: "Emamnuel korir", email: "2", position: "lec", testimony: "good" },
-  { name: "Emamnuel ", email: "1", position: "dev", testimony: "great" },
+const test = [
+  {
+    name: "Emamnuel korir",
+    email: "2",
+    position: "lec",
+    testimony: "good",
+    id: 1,
+  },
+  { name: "Emamnuel ", email: "1", position: "dev", testimony: "great", id: 2 },
 ];
+const testifieer = {
+  name: "Emamnuel ",
+  email: "",
+  position: "dev",
+  testimony: "great",
+};
 export default function Mytestimonial() {
+  const [testifiers, setTestifier] = useState([test]);
+  const [testif, settestif] = useState({ testifieer });
+
   const [addTestimony, setaddTestimony] = useState(false);
+
+  function handleForm(mytestifier) {
+    settestif(mytestifier);
+    setTestifier({ ...testifiers, mytestifier });
+    console.log(mytestifier);
+    console.log(testif);
+  }
 
   function handleAddTestimony() {
     setaddTestimony(true);
@@ -22,7 +44,7 @@ export default function Mytestimonial() {
       </div>
       <AddTestimonial onAddtest={handleAddTestimony} />
       <TestimonialCard testifiers={testifiers} />
-      {addTestimony && <Form />}
+      {addTestimony && <Form onsubmit={handleForm} />}
     </div>
   );
 }
@@ -31,14 +53,21 @@ function TestimonialCard({ testifiers }) {
   console.log(testifiers);
   return (
     <div className={styles.tesiCard}>
-      {testifiers.map((testifier) => (
-        <PersonCard key={testifier.email} testifier={testifier} />
+      {testifiers.map((person) => (
+        <PersonCard
+          //   name={name} { name, position, testimony, email }
+          //   position={position}
+          //   testimony={testimony}
+          //   testifier={email}
+          person={person}
+          key={person.id}
+        />
       ))}
-      ;
-      <PersonCard />
+      ;{/* <PersonCard /> */}
     </div>
   );
 }
+
 function AddTestimonial({ onAddtest }) {
   return (
     <div className={styles.testmonialBtn}>
@@ -53,9 +82,9 @@ function AddTestimonial({ onAddtest }) {
   );
 }
 
-function PersonCard({ testifier }) {
-  console.log(`${testifier} hello`);
-  const { name, position, testimony } = testifier;
+function PersonCard({ email, name, position, testimony }) {
+  //   console.log(`${email} hello`);
+  //   const { name, position, testimony } = testifier;
   return (
     <div className={styles.personCard}>
       <div className={styles.review}>
@@ -63,8 +92,9 @@ function PersonCard({ testifier }) {
           <ion-icon name="chatbubble-ellipses-outline"></ion-icon> Testimony
         </h3>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est,
-          pariatur maiores. Voluptate incidunt aut dolorum.
+          {testimony}
+          {/* Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est,
+          pariatur maiores. Voluptate incidunt aut dolorum. */}
         </p>
         <p className={styles.ratings}>⭐⭐⭐⭐⭐</p>
       </div>
@@ -72,7 +102,11 @@ function PersonCard({ testifier }) {
         <div className={styles.nameInitials}>KE</div>
         <div>
           <h2>{name}</h2>
-          <p>designation</p>
+          <p>{email}</p>
+          <p>
+            {/* designation */}
+            {position}
+          </p>
         </div>
       </div>
     </div>
@@ -82,7 +116,25 @@ function PersonCard({ testifier }) {
 // **************
 // Testimonial Form
 
-function Form() {
+function Form({ onsubmit }) {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [position, setpos] = useState("");
+  const [testimony, settestimony] = useState("");
+
+  function handleSubmision(e) {
+    e.preventDefault();
+    onsubmit(mytestifier);
+    // return { ...testifier, name, email, position, testimony };
+  }
+
+  const mytestifier = {
+    name: name,
+    email: email,
+    position: position,
+    testimony: testimony,
+  };
+
   return (
     <>
       <div className={styles.form}>
@@ -94,23 +146,41 @@ function Form() {
         <form className={styles.myform}>
           <div>
             <label>Full Name</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setname(e.value)}
+            />
           </div>
           <div>
             <label>Email</label>
-            <input type="email" />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setemail(e.value)}
+            />
           </div>
           <div>
             <label>Position</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={position}
+              onChange={(e) => setpos(e.value)}
+            />
           </div>
           <div>
             <label>Testimonial</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={testimony}
+              onChange={(e) => settestimony(e.value)}
+            />
           </div>
           <div className={styles.formBtn}>
             <button>Cancel</button>
-            <button type="submit">Submit</button>
+            <button type="submit" onClick={(e) => handleSubmision(e)}>
+              Submit
+            </button>
           </div>
         </form>
       </div>
