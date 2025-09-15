e323cf26edf3569eab2400ef1ba34883063e0f5e
iamkorir3 <korir5956@gmail.com>
2025-09-15 23:34:56 +0300

    working on skills page

---
+++ src/components/Myskills.jsx
@@ -0,0 +1,17 @@
+export default function Myskills() {
+  return (
+    <div>
+      <SkillCard />
+    </div>
+  );
+}
+
+function SkillCard() {
+  return (
+    <div>
+      <h3>HTML</h3>
+      <p>logo</p>
+      <div>line</div>
+    </div>
+  );
+}
