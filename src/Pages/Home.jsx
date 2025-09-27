adea1796ad5bd24bc5f9a92ecf9733875384d358
iamkorir3 <korir5956@gmail.com>
2025-09-27 17:03:30 +0300

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import Homepage from "../components/Homepage";
 import Navigation from "../components/Navigation";
 import styles from "./Home.module.css";
+import Aboutcompo from "../components/Aboutcompo";
+
 function Home() {
   return (
     <div className={styles.container}>
       <Navigation />
       <Homepage />
+      <Aboutcompo />
     </div>
   );
 }
